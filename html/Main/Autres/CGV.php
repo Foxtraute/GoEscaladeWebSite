81408ae48b45f6f6739121c11faa171d7f42a476
Foxtraute <117092413+Foxtraute@users.noreply.github.com>
2023-01-11 21:24:24 +0100

    Add files via upload

---
+++ html/Main/Autres/CGV.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Go Escalad​e !, Panier">
+    <meta name="description" content="">
+    <title>CGV</title>
+    <link rel="stylesheet" href="../../nicepage.css" media="screen">
+    <link rel="stylesheet" href="Panier.css" media="screen">
+    <script class="u-script" type="text/javascript" src="../../jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="../../nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.0.7, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i">
+    <meta property="og:title" content="Go Escalade">
+    <meta property="og:description" content="Go Escalade, le leader francophone de vente de matériel escalade">
+    <meta property="og:type" content="website">
+  </head>
+</head>
+<body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-image u-sticky u-header" id="sec-aa9f" data-image-width="1920" data-image-height="1080"><div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-white u-title u-text-1" data-href="/main/Go-Escalade.php" data-target="_blank"> Go Escalad​e !</h1>
+        <div class="u-image u-image-circle u-image-contain u-opacity u-opacity-80 u-preserve-proportions u-image-1" alt="" data-image-width="600" data-image-height="600" data-href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" data-target="_blank" rel="nofollow"></div>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="sec-1a1e">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-1">Conditions Générales de Vente</h1>
+        <p style="text-align: left;">
+          <span style="line-height: 2.0;">Fuerit et sis pervenire sortiri diem habere absurdum certam non provinciam licebit Martiis est denique Nam provinciam non diem diem toto sortitus ei habebit et paludatus Quo Februario Nam habebit provincia sis toto provincia provincia decreta est Martiis consulatu Proficiscetur Quo habere denique et diem sine toto et non Quo Proficiscetur designatus Sortietur est ante Martiis fuerit certam fuerit non non ei antequam sine Quo antequam provincia Proficiscetur et est paludatus ante sine fuerit ante antequam pervenire Quo ei non Martiis ante.
+          </span>
+          <p style="text-align: left;">
+            <span style="line-height: 2.0;">Consumendo quod exitialis rerum alimentorum flumen alimentorum clausos aerumnas horrebant tamen horrebant angebat frumenta vehementer iam alimentorum tamen cibos quidem Isauri solitarum adfluebant aerumnas solitarum iam frumenta clausos per copiis per vehebant navigiis frumenta cibos vero ipsi quod solitarum inediae ipsi aerumnas solitarum solitarum horrebant adfluebant ipsi copiis iam frumenta vero navigiis aerumnas tamen ipsi flumen vehebant quae angebat frumenta vehementer rerum cibos copiis iam quod consumendo clausos tamen vehebant flumen cibos cibos clausos quod quae solitarum clausos consumendos. Levi proximo sine ad adorti adorti praefecti senem resticulis atque divaricaturn atque spiramento divaricaturn divaricaturn haec Montium post sunt spiramento haec ad divaricaturn divertebat ad corpore divertebat militares spiramento saepe spiramento adorti proximo senem avidi divertebat usque proximo morbosum usque morbosum innexis innexis adorti militares spiramento avidi ad post ad militares in turbarum proximo usque eius sunt in avidi hirsutis morati praefecti primum proximo qui hirsutis proximo eius Montium usque post primum usque traxere ad atque praefecti adorti turbarum post innexis adorti corpore primum spiramento praetorium eius proximo spiramento qui saepe divertebat eius et resticulis avidi ullo corpore haec usque.</span>
+            <p style="text-align: left;">
+              <span style="line-height: 2.0;">Nam esse ut atque enim circumfluere atque nec nullus ratione fiducia tyrannorum homines sollicita omnia atque de pro tyrannorum pro Nam quemquam ratione sollicita deorum nec quemquam omnibus fiducia disputabunt ab habent est amicitia locus quam ut fiducia rerum si vivere pro amicitia ullo neque neque diligat quis nec nullus amicitia rerum sollicita cognitam vivere hominum est ipse in cognitam in habent tyrannorum omnibus fides quis quando.
+              </span>
+              <p style="text-align: left;">
+                <span style="line-height: 2.0;">Sed enim adferunt spem fructus in in Nec si enim quod et vetustas quod res cuius tamquam herbis in sed quae cuius hoc eo feci consuetudinis loco et Nec vetustas ut enim feci vis Nec quod in Nec valet in animal est nemo conservanda etiam non fructus et est quam intractato enim iis quod hoc conservanda diutius et adferunt est ut mentionem utatur fructus silvestribus montuosis eo in non vetustas sumus tamquam nemo herbis si cuius quibus feci appareat spem quin et Quin appareat feci quin iis silvestribus tamquam autem libentius in etiam tamen enim illae intractato in impediat valet.</span>
+    </div>
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-cbff"><div class="u-clearfix u-sheet u-sheet-1">
+      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-1">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-1" href="../autres/CGV.php">CGV</a>
+      </h5>
+      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-2">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-2" href="../autres/CGU.php">CGU</a>
+      </h5>
+      <p class="u-small-text u-text u-text-variant u-text-3"> GoEscalade ! - SARL au capital de 15 000€ Siège Social - 13 Rue De La Serte 75019 PARIS&nbsp;© 2016 GoEscalade !</p>
+    </div></footer>
+  </section>
+
+</body></html>
---
+++ html/Main/Go-Escalade.php
@@ -0,0 +1,167 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="fr"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Go Escalad​e !">
+    <meta name="description" content="Le meilleur site d'escalade">
+    <title>Go Escalade</title>
+    <link rel="stylesheet" href="../nicepage.css" media="screen">
+<link rel="stylesheet" href="Go-Escalade.css" media="screen">
+    <script class="u-script" type="text/javascript" src="../jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="../nicepage.js" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i">
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Go Escalade">
+    <meta property="og:description" content="Go Escalade, le leader francophone de vente de matériel escalade">
+    <meta property="og:type" content="website">
+  </head>
+
+  <body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-image u-sticky u-header" id="sec-aa9f" data-image-width="1920" data-image-height="1080"><div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-white u-title u-text-1" data-href="/main/Go-Escalade.php" data-target="_blank"> Go Escalad​e !</h1>
+        <div class="u-image u-image-circle u-image-contain u-opacity u-opacity-80 u-preserve-proportions u-image-1" alt="" data-image-width="600" data-image-height="600" data-href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" data-target="_blank" rel="nofollow"></div>
+      </div></header>
+      <section class="u-clearfix u-custom-color-1 u-section-1" id="sec-4492">
+      <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-size-34-lg u-size-34-md u-size-34-sm u-size-34-xs u-size-39-xl u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h4 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-1"> "Si tu veux atteindre le sommet, n'observe pas la montagne, escalade-là"</h4>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-size-12-xl u-size-14-lg u-size-14-md u-size-14-sm u-size-14-xs u-layout-cell-2" data-href="Compte-et-Paiement/Connexion.php" title="Connexion" data-target="_blank">
+                <div class="u-container-layout u-container-layout-2">
+                  <h5 class="u-align-center u-text u-text-default u-text-font u-text-2">Connexion</h5>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-size-12-lg u-size-12-md u-size-12-sm u-size-12-xs u-size-9-xl u-layout-cell-3" data-href="/main/produits/panier.php" title="Panier" data-target="_blank">
+              <div class="u-container-layout u-container-layout-2">
+                  <h5 class="u-align-center u-text u-text-default u-text-font u-text-2">Panier</h5>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-white u-section-2" id="sec-cd9a">
+      <div class="u-clearfix u-sheet u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-sheet-1">
+        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-col">
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-image u-layout-cell u-size-60 u-image-1" data-image-width="1920" data-image-height="1080">
+                    <div class="u-border-1 u-border-black u-container-layout u-container-layout-1">
+                      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-custom-color-1 u-text-default u-text-1">Chaussons</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-hover-feature u-image u-layout-cell u-size-20 u-image-2" data-image-width="305" data-image-height="472" data-href="/main/produits/chaussons-hommes.php">
+                    <div class="u-border-1 u-border-black u-border-no-right u-border-no-top u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-4">
+                      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-4">Hommes</h5>
+                    </div>
+                  </div>
+                  <div class="u-container-style u-hover-feature u-image u-layout-cell u-size-20 u-image-3" data-image-width="351" data-image-height="605" data-href="/main/produits/chaussons-femmes.php">
+                    <div class="u-border-1 u-border-black u-border-no-left u-border-no-top u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-4">
+                      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-4">Femmes</h5>
+                    </div>
+                  </div>
+                  <div class="u-container-style u-hover-feature u-image u-layout-cell u-size-20 u-image-4" data-image-width="397" data-image-height="611" data-href="/main/produits/chaussons-enfants.php">
+                    <div class="u-border-1 u-border-black u-border-no-left u-border-no-top u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-4">
+                      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-4">Enfants</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="carousel-bd35" data-interval="5000" data-u-ride="carousel" class="u-carousel u-slider u-slider-1">
+          <ol class="u-absolute-hcenter u-carousel-indicators u-hidden u-carousel-indicators-1">
+            <li data-u-target="#carousel-bd35" class="u-active u-grey-30" data-u-slide-to="0"></li>
+            <li data-u-target="#carousel-bd35" class="u-grey-30" data-u-slide-to="1"></li>
+            <li data-u-target="#carousel-bd35" class="u-grey-30" data-u-slide-to="2"></li>
+          </ol>
+          <div class="u-carousel-inner" role="listbox">
+            <div class="u-active u-align-center u-carousel-item u-container-style u-image u-shading u-slide u-image-5" data-image-width="1280" data-image-height="853" data-href="/main/produits/baudriers.php">
+              <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-container-layout-5">
+                <h2 class="u-align-center-sm u-align-center-xs u-custom-font u-font-ubuntu u-text u-text-default u-text-5">Baudriers</h2>
+              </div>
+            </div>
+            <div class="u-align-center u-carousel-item u-container-style u-image u-shading u-slide u-image-6" data-image-width="600" data-image-height="400" data-href="/main/produits/magnesite.php">
+              <div class="u-container-layout u-valign-middle u-container-layout-6">
+                <h2 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-6">Magnesite</h2>
+              </div>
+            </div>
+            <div class="u-align-center u-carousel-item u-container-style u-image u-shading u-slide u-image-7" data-image-width="1000" data-image-height="615" data-href="/main/produits/promotions.php">
+              <div class="u-container-layout u-valign-middle u-container-layout-7">
+                <h2 class="u-custom-font u-font-ubuntu u-text u-text-default u-text-7">Promotions</h2>
+              </div>
+            </div>
+          </div>
+          <a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-grey-60 u-icon-circle u-spacing-9 u-carousel-control-1" href="#carousel-bd35" role="button" data-u-slide="prev">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 477.175 477.175"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
+                    c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path></svg>
+            </span>
+            <span class="sr-only">+Previous</span>
+          </a>
+          <a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-grey-60 u-icon-circle u-spacing-9 u-carousel-control-2" href="#carousel-bd35" role="button" data-u-slide="next">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 477.175 477.175"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5
+                    c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"></path></svg>
+            </span>
+            <span class="sr-only">+Next</span>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-image u-shading u-section-3" src="" data-image-width="1024" data-image-height="640" id="sec-b4fd">
+      <div class= "u-absolute-hcenter u-clearfix u-sheet u-valign-middle-md u-valign-middle-sm u-valign-middle-xl u-valign-middle-xs u-sheet-1" >
+        <div class="u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-left-cell u-size-21-lg u-size-21-md u-size-21-sm u-size-28-xs u-size-28-xl u-layout-cell-1">
+                <div class="u-container-layout u-valign-top u-container-layout-1">
+                  <h2 class="u-align-justify u-text u-text-1">Go Escalade !</h2>
+                  <p class="u-align-left u-text u-text-2">13 Rue de la Serte, 75019 Paris</p>
+                  <p class="u-align-left u-text u-text-3">0959385718</p>
+                  <p class="u-align-left u-text u-text-4">support@go-escalade.fr</p>
+                  <p class="u-align-left u-text u-text-5">
+                    <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-text-hover-white u-btn-1" href="autres/Contact.php">Formulaire de contact</a>
+                  </p>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-right-cell u-size-27-xl u-size-39-lg u-size-39-md u-size-39-sm u-size-39-xs u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <h2 class="u-align-center u-text u-text-6">Qui Sommes nous ?</h2>
+                  <p class="u-align-justify u-text u-text-7">Go Escalade est avant tout un groupe d'amis passioné par l'escalade de voie et de bloc. Décidant de vouloir partager leur expérience en choix de matériel pour la grimpe ! Passionné comme confirmé, vous trouverez tout une liste de matériel et accessoires pour la grimpe à votre diposition sur Go-Escalade.fr !</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="u-align-left u-clearfix u-footer u-grey-80 u-footer" id="sec-cbff"><div class="u-clearfix u-sheet u-sheet-1"> 
+      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-1">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-1" href="../main/autres/CGV.php">CGV</a>
+      </h5>
+      <h5 class="u-align-center u-custom-font u-font-ubuntu u-text u-text-body-alt-color u-text-default u-text-2">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-2" href="../main/autres/CGU.php">CGU</a>
+      </h5>
+      <p class="u-small-text u-text u-text-variant u-text-3"> GoEscalade ! - SARL au capital de 15 000€ Siège Social - 13 Rue De La Serte 75019 PARIS&nbsp;</p>
+      <?php echo 'IP du client: '.$_SERVER['REMOTE_ADDR']; echo " |  Bonjour " . $_SESSION['username'] ;?>
+    </div></footer> 
+  </section>
+</body>
+</html>
